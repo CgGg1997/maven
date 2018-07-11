0c32553985e9a97f692c9d99c80082a590fd8a27
yuanerha <471699340@qq.com>
2018-07-11 20:30:54 +0800

    第一次上传

---
+++ src/main/java/org/java/service/impl/InfServiceImpl.java
@@ -0,0 +1,32 @@
+package org.java.service.impl;
+
+import org.java.dao.InfMapper;
+import org.java.service.InfService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by 马果 on 2018/7/8.
+ */
+@Service("infService")
+public class InfServiceImpl implements InfService {
+
+    @Autowired
+    private InfMapper mapper;
+
+    @Transactional
+    @Override
+    public void add(Map<String, Object> m) {
+
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public List<Map<String, Object>> getList() {
+        return mapper.getList();
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<body>
+<a href="load.do">进入</a>
+</body>
+</html>
